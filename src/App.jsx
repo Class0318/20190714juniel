059064a0b264b4f8b83acda2bd9b7657fd754918
xixi <xixi@qq.com>
2019-07-20 11:27:05 +0800

    aa

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import React,{Component} from 'react'
+import {Button,message} from 'antd'
 
-export default class App extends Component{
+import {BrowserRouter,HashRouter,Switch,Route} from 'react-router-dom' 
+import Login from './pages/login/login'
+import Admin from './pages/admin/admin'   
+
+//应用根组件
+class App extends Component{
     render(){
         return(
-            <div>App</div>
+            <BrowserRouter>
+               <Switch>
+                   <Route path='/login' component={Login}/>
+                   <Route path='/' component={Admin}/> 
+               </Switch>
+            </BrowserRouter>
         )
     }
 }
+
+
+export default App
+
